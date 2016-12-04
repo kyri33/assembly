25d719242fe7f3e739fd917f4d5f93571af65c62
Kyri Ioulianou <Kyri@kyris-air.uth.local>
2016-12-04 17:00:46 +0200

    Initial commit

---
+++ HelloWorld/HelloWorld.asm
@@ -0,0 +1,15 @@
+section .data ; data segment
+	msg	db	"Hello World!", 0x0a
+
+section .text
+global _start
+_start
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, 14
+	int 0x80
+
+	mov eax, 1
+	mov ebx, 0
+	int 0x80
